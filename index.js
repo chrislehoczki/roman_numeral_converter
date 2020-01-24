57950c88cac6cb086882ee81c0095da2355ab762
Chris Phillips <chris.is.phillips@gmail.com>
2020-01-24 16:37:10 +0000

    Naming conventions and syntax

--- index.js
+++ index.js
@@ -3,28 +3,30 @@ export const inputError = new Error(
 );
 export const typeError = new TypeError("Input provided was not a number");
 
-function convertUnitValue(num, placeValue) {
-  num = +num;
-  const lookup = [["I", "V", "X"], ["X", "L", "C"], ["C", "D", "M"], ["M"]];
-  const [one, five, ten] = lookup[placeValue];
-  const numerals = {
-    "0-3": one.repeat(num),
-    "4": `${one}${five}`,
-    "5": five,
-    "6-8": `${five}${one.repeat(num % 5)}`,
-    "9": `${one}${ten}`
-  };
-  return numerals[numerals[num] ? num : num < 4 ? "0-3" : "6-8"];
-}
+const convertValueToNumeral = (num, exponent) => {
+  const baseTenNumerals = [
+    ["I", "V", "X"],
+    ["X", "L", "C"],
+    ["C", "D", "M"],
+    ["M"]
+  ];
+  const [one, five, ten] = baseTenNumerals[exponent];
+  if (num < 4) return one.repeat(num);
+  if (num === 4) return `${one}${five}`;
+  if (num === 5) return five;
+  if (num > 5 && num < 9) return `${five}${one.repeat(num % 5)}`;
+  if (num === 9) return `${one}${ten}`;
+};
 
-export function convertRomanNumeral(val) {
+export const convertRomanNumeral = val => {
   if (isNaN(val)) throw typeError;
   if (val < 1 || val > 3999) throw inputError;
   return val
     .toString()
     .split("")
+    .map(num => +num)
     .reverse()
-    .map(convertUnitValue)
+    .map(convertValueToNumeral)
     .reverse()
     .join("");
-}
+};
