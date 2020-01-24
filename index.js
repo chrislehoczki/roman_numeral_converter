5ba6275113d7fcb6bf43eab11ab1782ebdba82f2
Chris Phillips <chris.is.phillips@gmail.com>
2020-01-24 12:37:49 +0000

    Reformatted tab spacing

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-export const inputError = new Error("Input provided was not between 1 and 3999")
-export const typeError = new TypeError("Input provided was not a number")
+export const inputError = new Error(
+  "Input provided was not between 1 and 3999"
+);
+export const typeError = new TypeError("Input provided was not a number");
 
 function convertUnitValue(num, placeValue) {
-	num = +num
-	const lookup = [["I", "V", "X"], ["X", "L", "C"], ["C", "D", "M"], ["M"]]
-	const [one, five, ten] = lookup[placeValue]
-	const numerals = {
-		"9": `${one}${ten}`,
-		"5": five,
-		"4": `${one}${five}`,
-		"0-3": one.repeat(num),
-		"6-8": `${five}${one.repeat(num % 5)}`,
-	}
-	return numerals[numerals[num] ? num : num < 4 ? "0-3" : "6-8"]
+  num = +num;
+  const lookup = [["I", "V", "X"], ["X", "L", "C"], ["C", "D", "M"], ["M"]];
+  const [one, five, ten] = lookup[placeValue];
+  const numerals = {
+    "9": `${one}${ten}`,
+    "5": five,
+    "4": `${one}${five}`,
+    "0-3": one.repeat(num),
+    "6-8": `${five}${one.repeat(num % 5)}`
+  };
+  return numerals[numerals[num] ? num : num < 4 ? "0-3" : "6-8"];
 }
 
 export function convertRomanNumeral(val) {
-	if (isNaN(val)) throw typeError
-	if (val < 1 || val > 3999) throw inputError
-	return val
-		.toString()
-		.split("")
-		.reverse()
-		.map(convertUnitValue)
-		.reverse()
-		.join("")
+  if (isNaN(val)) throw typeError;
+  if (val < 1 || val > 3999) throw inputError;
+  return val
+    .toString()
+    .split("")
+    .reverse()
+    .map(convertUnitValue)
+    .reverse()
+    .join("");
 }
--- index.spec.js
+++ index.spec.js
@@ -1,56 +1,56 @@
-import { convertRomanNumeral, inputError, typeError } from "./index"
+import { convertRomanNumeral, inputError, typeError } from "./index";
 
 describe("Roman numeral parser", () => {
-	describe("Input checks", () => {
-		it("Throws an error when input is less than 1", () => {
-			expect(() => convertRomanNumeral(0)).toThrow(inputError)
-		})
-		it("Throws an error when input is greater than 3999", () => {
-			expect(() => convertRomanNumeral(4000)).toThrow(inputError)
-		})
-		it("Throws an error when input is not a number", () => {
-			expect(() => convertRomanNumeral("A String")).toThrow(typeError)
-		})
-	})
-	describe("Place value 1", () => {
-		const tests = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-		tests.forEach((numeral, i) =>
-			it(`Expect ${i + 1} to equal ${numeral}`, () =>
-				expect(convertRomanNumeral(i + 1)).toEqual(numeral))
-		)
-	})
-	describe("Place value 10", () => {
-		const tests = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-		tests.forEach((numeral, i) =>
-			it(`Expect ${(i + 1) * 10} to equal ${numeral}`, () =>
-				expect(convertRomanNumeral((i + 1) * 10)).toEqual(numeral))
-		)
-	})
-	describe("Place value 100", () => {
-		const tests = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-		tests.forEach((numeral, i) =>
-			it(`Expect ${(i + 1) * 100} to equal ${numeral}`, () =>
-				expect(convertRomanNumeral((i + 1) * 100)).toEqual(numeral))
-		)
-	})
-	describe("Place value 1000", () => {
-		const tests = ["M", "MM", "MMM"]
-		tests.forEach((numeral, i) =>
-			it(`Expect ${(i + 1) * 1000} to equal ${numeral}`, () =>
-				expect(convertRomanNumeral((i + 1) * 1000)).toEqual(numeral))
-		)
-	})
-	describe("Smoke test range of values", () => {
-		const tests = [
-			[68, "LXVIII"],
-			[649, "DCXLIX"],
-			[891, "DCCCXCI"],
-			[2014, "MMXIV"],
-			[3999, "MMMCMXCIX"],
-		]
-		tests.forEach(([input, expected], i) =>
-			it(`Expect ${input} to equal ${expected}`, () =>
-				expect(convertRomanNumeral(input)).toEqual(expected))
-		)
-	})
-})
+  describe("Input checks", () => {
+    it("Throws an error when input is less than 1", () => {
+      expect(() => convertRomanNumeral(0)).toThrow(inputError);
+    });
+    it("Throws an error when input is greater than 3999", () => {
+      expect(() => convertRomanNumeral(4000)).toThrow(inputError);
+    });
+    it("Throws an error when input is not a number", () => {
+      expect(() => convertRomanNumeral("A String")).toThrow(typeError);
+    });
+  });
+  describe("Place value 1", () => {
+    const tests = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    tests.forEach((numeral, i) =>
+      it(`Expect ${i + 1} to equal ${numeral}`, () =>
+        expect(convertRomanNumeral(i + 1)).toEqual(numeral))
+    );
+  });
+  describe("Place value 10", () => {
+    const tests = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    tests.forEach((numeral, i) =>
+      it(`Expect ${(i + 1) * 10} to equal ${numeral}`, () =>
+        expect(convertRomanNumeral((i + 1) * 10)).toEqual(numeral))
+    );
+  });
+  describe("Place value 100", () => {
+    const tests = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    tests.forEach((numeral, i) =>
+      it(`Expect ${(i + 1) * 100} to equal ${numeral}`, () =>
+        expect(convertRomanNumeral((i + 1) * 100)).toEqual(numeral))
+    );
+  });
+  describe("Place value 1000", () => {
+    const tests = ["M", "MM", "MMM"];
+    tests.forEach((numeral, i) =>
+      it(`Expect ${(i + 1) * 1000} to equal ${numeral}`, () =>
+        expect(convertRomanNumeral((i + 1) * 1000)).toEqual(numeral))
+    );
+  });
+  describe("Smoke test range of values", () => {
+    const tests = [
+      [68, "LXVIII"],
+      [649, "DCXLIX"],
+      [891, "DCCCXCI"],
+      [2014, "MMXIV"],
+      [3999, "MMMCMXCIX"]
+    ];
+    tests.forEach(([input, expected], i) =>
+      it(`Expect ${input} to equal ${expected}`, () =>
+        expect(convertRomanNumeral(input)).toEqual(expected))
+    );
+  });
+});
