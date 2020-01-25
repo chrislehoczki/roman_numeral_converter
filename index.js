15d1c0dec30316ddd24af8b1cddf09b615ca97c1
Chris Phillips <chris.is.phillips@gmail.com>
2020-01-25 16:16:47 +0000

    Implemented class from original technical task spec

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ export const inputError = new Error(
 );
 export const typeError = new TypeError("Input provided was not a number");
 
-const convertValueToNumeral = (val, exponent) => {
-  const baseTenNumerals = [
-    ["I", "V", "X"],
-    ["X", "L", "C"],
-    ["C", "D", "M"],
-    ["M", null, null]
-  ];
-  const [one, five, ten] = baseTenNumerals[exponent];
-  if (val < 4) return one.repeat(val);
-  if (val === 4) return `${one}${five}`;
-  if (val === 5) return five;
-  if (val > 5 && val < 9) return `${five}${one.repeat(val % 5)}`;
-  if (val === 9) return `${one}${ten}`;
-};
-
-export const convertRomanNumeral = val => {
-  if (isNaN(val)) throw typeError;
-  if (val < 1 || val > 3999) throw inputError;
-  return val
-    .toString()
-    .split("")
-    .map(num => +num)
-    .reverse()
-    .map(convertValueToNumeral)
-    .reverse()
-    .join("");
-};
+export class RomanNumeralGenerator {
+  generate(val) {
+    if (isNaN(val)) throw typeError;
+    if (val < 1 || val > 3999) throw inputError;
+    return val
+      .toString()
+      .split("")
+      .map(num => +num)
+      .reverse()
+      .map(this.convertValueToNumeral)
+      .reverse()
+      .join("");
+  }
+  convertValueToNumeral(val, exponent) {
+    const baseTenNumerals = [
+      ["I", "V", "X"],
+      ["X", "L", "C"],
+      ["C", "D", "M"],
+      ["M", null, null]
+    ];
+    const [one, five, ten] = baseTenNumerals[exponent];
+    if (val < 4) return one.repeat(val);
+    if (val === 4) return `${one}${five}`;
+    if (val === 5) return five;
+    if (val > 5 && val < 9) return `${five}${one.repeat(val % 5)}`;
+    if (val === 9) return `${one}${ten}`;
+  }
+}
--- index.spec.js
+++ index.spec.js
@@ -1,43 +1,45 @@
-import { convertRomanNumeral, inputError, typeError } from "./index";
+import { RomanNumeralGenerator, inputError, typeError } from "./index";
 
-describe("Roman numeral parser", () => {
+const numeralGenerator = new RomanNumeralGenerator();
+
+describe("RomanNumeralGenerator", () => {
   describe("Input checks", () => {
     it("Throws an error when input is less than 1", () => {
-      expect(() => convertRomanNumeral(0)).toThrow(inputError);
+      expect(() => numeralGenerator.generate(0)).toThrow(inputError);
     });
     it("Throws an error when input is greater than 3999", () => {
-      expect(() => convertRomanNumeral(4000)).toThrow(inputError);
+      expect(() => numeralGenerator.generate(4000)).toThrow(inputError);
     });
     it("Throws an error when input is not a number", () => {
-      expect(() => convertRomanNumeral("A String")).toThrow(typeError);
+      expect(() => numeralGenerator.generate("A String")).toThrow(typeError);
     });
   });
   describe("Handles 10^0 cases", () => {
     const tests = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
     tests.forEach((numeral, i) =>
       it(`Expect ${i + 1} to equal ${numeral}`, () =>
-        expect(convertRomanNumeral(i + 1)).toEqual(numeral))
+        expect(numeralGenerator.generate(i + 1)).toEqual(numeral))
     );
   });
   describe("Handles 10^1 cases", () => {
     const tests = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
     tests.forEach((numeral, i) =>
       it(`Expect ${(i + 1) * 10} to equal ${numeral}`, () =>
-        expect(convertRomanNumeral((i + 1) * 10)).toEqual(numeral))
+        expect(numeralGenerator.generate((i + 1) * 10)).toEqual(numeral))
     );
   });
   describe("Handles 10^2 cases", () => {
     const tests = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
     tests.forEach((numeral, i) =>
       it(`Expect ${(i + 1) * 100} to equal ${numeral}`, () =>
-        expect(convertRomanNumeral((i + 1) * 100)).toEqual(numeral))
+        expect(numeralGenerator.generate((i + 1) * 100)).toEqual(numeral))
     );
   });
   describe("Handles 10^3 cases", () => {
     const tests = ["M", "MM", "MMM"];
     tests.forEach((numeral, i) =>
       it(`Expect ${(i + 1) * 1000} to equal ${numeral}`, () =>
-        expect(convertRomanNumeral((i + 1) * 1000)).toEqual(numeral))
+        expect(numeralGenerator.generate((i + 1) * 1000)).toEqual(numeral))
     );
   });
   describe("Handles range of values", () => {
@@ -50,7 +52,7 @@ describe("Roman numeral parser", () => {
     ];
     tests.forEach(([input, expected], i) =>
       it(`Expect ${input} to equal ${expected}`, () =>
-        expect(convertRomanNumeral(input)).toEqual(expected))
+        expect(numeralGenerator.generate(input)).toEqual(expected))
     );
   });
 });
